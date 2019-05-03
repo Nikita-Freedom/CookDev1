f8db9104c0a5fa41c330f69ada39505fe2da71fc
Nikita-Freedom <44145716+Nikita-Freedom@users.noreply.github.com>
2019-05-03 16:43:45 +0200

    add

---
+++ app/src/main/java/ru/startandroid/cookdev1/AbcMidFragment.java
@@ -0,0 +1,12 @@
+package ru.startandroid.cookdev1;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+
+public class AbcMidFragment extends Fragment {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
