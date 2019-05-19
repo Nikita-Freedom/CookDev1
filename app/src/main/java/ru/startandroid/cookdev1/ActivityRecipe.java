5c7b6f5e99c0f5fe4490c05bda61a12fe0b50247
Nikita-Freedom <44145716+Nikita-Freedom@users.noreply.github.com>
2019-05-19 02:15:37 +0200

    add

--- app/src/main/java/ru/startandroid/cookdev1/ActivityRecipe.java
+++ app/src/main/java/ru/startandroid/cookdev1/ActivityRecipe.java
@@ -2,6 +2,8 @@ package ru.startandroid.cookdev1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.webkit.WebView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -34,6 +36,7 @@ public class ActivityRecipe extends AppCompatActivity {
     ImageView mImage9;
     ImageView mImage10;
     ImageView mImage11;
+    WebView myWebView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,5 +46,14 @@ public class ActivityRecipe extends AppCompatActivity {
         ArrayList<Recipe> activityRecipes = new ArrayList<>();
         activityRecipes.add(new Recipe(R.drawable.shaurma, R.drawable.podnos, R.drawable.ovenrec3, R.drawable.fondev4, R.drawable.oven, R.drawable.zapekanka3, R.drawable.zapekanka1, R.drawable.zepekanka2, R.drawable.tvorog, R.drawable.terka, R.drawable.vodakipit,
                 "ssss", "ssss", "aaaaaa", "wwww", "oooo", "jjjjj", "sss", "pppp", "mmmm", "zzz", "aqqaq"));
+        WebView myWebView = new WebView(getApplicationContext());
+        setContentView(myWebView);
+        myWebView.loadUrl("https://be1.ru/html-redaktor-online/?data=0JZzQAdN5AUb93Age91XkrpLw4h2mIPr");
+        myWebView.findViewById(R.id.webview);
+        String unencodedHtml =
+                "&lt;html&gt;&lt;body&gt;'%23' is the percent code for ‘#‘ &lt;/body&gt;&lt;/html&gt;";
+        String encodedHtml = Base64.encodeToString(unencodedHtml.getBytes(),
+                Base64.NO_PADDING);
+        myWebView.loadData(encodedHtml, "text/html", "base64");
     }
 }
