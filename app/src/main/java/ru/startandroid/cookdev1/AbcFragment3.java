ab4e7b064aec3a1e7cdf364acb94669dd5d40f43
teacher <milena1990_bk.ru@icloud.com>
2019-05-26 02:04:34 +0300

    d

--- app/src/main/java/ru/startandroid/cookdev1/AbcFragment3.java
+++ app/src/main/java/ru/startandroid/cookdev1/AbcFragment3.java
@@ -48,6 +48,16 @@ public class AbcFragment3 extends Fragment {
                     transaction.add(R.id.fragment_container, fragmentabc7);
                     transaction.commit();
                 }
+            });
+            Button button4 = (Button) v.findViewById(R.id.buttonabc3up);
+            button4.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    fragmentabc6 = new AbcFragment4();
+                    FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                    transaction.add(R.id.fragment_container, fragmentabc6);
+                    transaction.commit();
+                }
             });return v;
 
         }
